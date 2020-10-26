c025d22352594b91d2c0cb4fdf6486c2b558cf38
Alba De biase <debiasealba@gmail.com>
2020-10-26 17:02:39 +0100

    visualizzo menu hamburger

---
+++ main.js
@@ -0,0 +1,7 @@
+// Al click sull'icona dell'hamburger menu, deve essere visualizzato il menu mobile
+// e al click sull'icona "X" il menu mobile deve scomparire.
+// In altre parole, dovere scrivere solo il codice js necessario a far funzionare l'hamburger menu.
+
+al click sull'icona hamburger viene visualizzato il Menu
+$(".fa-bars").click();
+$(".header-right ul li").show();
